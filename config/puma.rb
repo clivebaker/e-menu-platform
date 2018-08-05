b27dba7f66c5a7bd41e191e6236f2d1d5787e1c9
Clive Baker <clive@clivebaker.com>
2018-08-05 20:56:35 +0100

    Demo

--- config/puma.rb
+++ config/puma.rb
@@ -1,26 +1,34 @@
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = app_dir
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
-# Default to production
-rails_env = ENV['RAILS_ENV'] || 'production'
-environment rails_env
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
-if rails_env == "production"
-  workers 4
-  threads 1, 16
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-  pidfile "#{shared_dir}/tmp/pids/puma.pid"
-  state_path "#{shared_dir}/tmp/pids/puma.state"
-else
-  port        ENV.fetch("PORT") { 3000 }
-end
-
-
-
-
-
-
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
