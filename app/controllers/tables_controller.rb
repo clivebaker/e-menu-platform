50bd1963727264f3586922b561758f2f97d70785
Clive Baker <clive@clivebaker.com>
2018-12-16 01:25:50 +0000

    apple pay

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -76,10 +76,10 @@ class TablesController < ApplicationController
 
     respond_to do |format|
       if error.present?
-        format.json { render json: {error: true} }
+        format.json { render json: {error: 'true'} }
         format.html {redirect_to table_pay_path(@table), alert: "There has been an error: #{e.message}"}
       else
-        format.json { render json: {error: false} }
+        format.json { render json: {error: 'false'} }
         format.html {redirect_to table_pay_path(@table), notice: "Thank You, your payment was successful."}
       end
     end
