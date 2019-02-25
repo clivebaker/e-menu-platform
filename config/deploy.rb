6f750dffb19c3c007f9c325714dc866001fe9281
Clive Baker <clive@clivebaker.com>
2019-02-25 11:47:12 +0000

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       execute 'sudo service emenu restart'
-    #  execute 'service emenu_sidekiq restart'
+      execute 'sudo service emenu_sidekiq restart'
 
     end
   end
