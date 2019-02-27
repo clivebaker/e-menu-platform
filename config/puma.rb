a506e3827f469a58fc7e8fec6be89a7dcd8fdd48
Clive Baker <clive@clivebaker.com>
2019-02-27 15:36:12 +0000

    heroku deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,46 +1,19 @@
-# frozen_string_literal: true
-
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-
-
-
-
-
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = app_dir
-
-
-
-rails_env = ENV['RAILS_ENV'] || 'production'
-environment rails_env
-
-
-
-if rails_env == "production"
-  workers 4
-  threads 1, 16
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-  pidfile "#{shared_dir}/tmp/pids/puma.pid"
-  state_path "#{shared_dir}/tmp/pids/puma.state"
-else
-  port        ENV.fetch("PORT") { 3000 }
-end
-
-
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -58,4 +31,4 @@ end
 # preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-#plugin :tmp_restart
+plugin :tmp_restart
