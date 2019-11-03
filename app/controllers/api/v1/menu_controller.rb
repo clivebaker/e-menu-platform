f7354268713b12ab7249a4587d36e85e3dd8c706
Clive Baker <clive@clivebaker.com>
2019-11-03 19:37:25 +0000

    cache

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -3,44 +3,53 @@ module Api
 		class MenuController < ApiController 
       def index
         restaurant = Restaurant.find(params[:id])
-        ret = Menu.arrange_serializable
-        ret = ret.select{|r| r['restaurant_id']==restaurant.id}
+  
 
-        ret2 = Menu.arrange_serializable do |parent, children|
-          MenuSerializer.new(parent).to_json
-          # ActiveModelSerializers::SerializableResource.new(parent, serializer: MenuSerializer).to_json
 
-        end
-        ret3 = Menu.arrange_serializable do |parent, children|
-          url = url_for(parent.image) if parent.image.attached?
-          {
-            id: parent.id,
-            restaurant_id: parent.restaurant_id,
-            spice_level_id: parent.spice_level_id,
-            menu_item_categorisation_id: parent.menu_item_categorisation_id,
-            prices: parent.prices,
-            available: parent.available,
-            calories: parent.calories,
-            ancestry:parent.ancestry,
-            node_type: parent.node_type,
-            price_a: parent.price_a,
-            price_b: parent.price_b,
-            nutrition: parent.nutrition,
-            provenance: parent.provenance,
-            custom_lists: custom_list(parent.custom_lists),
-            position: parent.position,
-            name: parent.name,
-            image: url,
-            description: parent.description,
-            children: children
-          }
-        end
-        # binding.pry
-
-        ret3 = ret3.select{|r| r[:restaurant_id]==restaurant.id}
-
-        render json: ret3
+        render json: menu_json(restaurant.id)
       end
+
+    def menu_json restaurant_id
+      Rails.cache.fetch("api/restaurant/#{restaurant_id}/menu", expires_in: 12.hours) do
+        return_json(restaurant_id)
+      end
+    end
+
+
+    def  return_json restaurant_id
+
+      ret3 = Menu.arrange_serializable do |parent, children|
+        url = url_for(parent.image) if parent.image.attached?
+        {
+          id: parent.id,
+          restaurant_id: parent.restaurant_id,
+          spice_level_id: parent.spice_level_id,
+          menu_item_categorisation_id: parent.menu_item_categorisation_id,
+          prices: parent.prices,
+          available: parent.available,
+          calories: parent.calories,
+          ancestry:parent.ancestry,
+          node_type: parent.node_type,
+          price_a: parent.price_a,
+          price_b: parent.price_b,
+          nutrition: parent.nutrition,
+          provenance: parent.provenance,
+          custom_lists: custom_list(parent.custom_lists),
+          position: parent.position,
+          name: parent.name,
+          image: url,
+          description: parent.description,
+          children: children
+        }
+      end
+      # binding.pry
+
+      ret3 = ret3.select{|r| r[:restaurant_id]==restaurant_id}
+
+    end
+
+
+
 
       def custom_list(lists)
         ret = []
