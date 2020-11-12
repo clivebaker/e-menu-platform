74ea1bf47e5666e898572fd727357227f38b541a
James Grant <1993jamesgrant@gmail.com>
2020-11-12 11:26:44 +0000

    Remove old folder

--- spec-OLD/factories/custom_list_items.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :custom_list_item do
-    name "MyString"
-custom_list nil
-price "9.99"
-description "MyText"
-  end
-
-end
--- spec-OLD/factories/restaurants.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :restaurant do
-  end
-end
