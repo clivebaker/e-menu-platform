4ccff145be6000f6dbc6f1e3a8d1267e6866e209
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-11-13 10:03:14 +0000

    Adds sidebar basic functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,27 +19,21 @@
 //= require bootstrap-datepicker
 //= require datatables
 
-
 // Sidebar open/close
 $(document).ready(function () {
-  $("#sidebar").mCustomScrollbar({
-      theme: "minimal"
+  $("#dismiss, .overlay").on("click", function () {
+    // hide sidebar
+    $("#sidebar").removeClass("active");
+    // hide overlay
+    $(".overlay").removeClass("active");
   });
 
-  $('#dismiss, .overlay').on('click', function () {
-      // hide sidebar
-      $('#sidebar').removeClass('active');
-      // hide overlay
-      $('.overlay').removeClass('active');
-  });
-
-  $('#sidebarCollapse').on('click', function () {
-      // open sidebar
-      $('#sidebar').addClass('active');
-      // fade in the overlay
-      $('.overlay').addClass('active');
-      $('.collapse.in').toggleClass('in');
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  $("#sidebarCollapse").on("click", function () {
+    // open sidebar
+    $("#sidebar").addClass("active");
+    // fade in the overlay
+    $(".overlay").addClass("active");
+    $(".collapse.in").toggleClass("in");
+    $("a[aria-expanded=true]").attr("aria-expanded", "false");
   });
 });
-
