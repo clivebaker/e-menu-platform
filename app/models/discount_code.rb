c2f7aaa474af713eeb2919d60fc9abce76f6a33f
James Grant <1993jamesgrant@gmail.com>
2020-12-01 17:36:34 +0000

    Add unit tests for discount codes

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -3,13 +3,19 @@
 class DiscountCode < ApplicationRecord
     belongs_to :restaurant, class_name: 'Restaurant'
     
-    scope :is_active?, -> { where("created_at < ? OR created_at IS NULL", DateTime.now) }
+    scope :is_active?, -> { where("expires_on > ? OR created_at IS NULL", DateTime.now) }
+    def self.types; ["PercentageOffBasketDiscountCode"]; end
     
     validates_presence_of :type, :code, :amount
     validates :code, :uniqueness => { constraint: is_active? }
     validates :code, length: { minimum: 3, maximum: 10 }
-
-    def self.types; ["PercentageOffBasketDiscountCode"]; end
+    validates :type, inclusion: { in: DiscountCode.types }
 
     def expiry_date; self.expires_on.presence || "Never"; end
+
+    def is_active?
+      self.expires_on > DateTime.now || self.expires_on.nil?
+    end
+
+    private
 end
