1dfbd33416a26c3d4cc7e983fa7839d73bf12069
James Grant <1993jamesgrant@gmail.com>
2020-12-04 18:32:32 +0000

    Discount code bug fixes

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -3,7 +3,7 @@
 class DiscountCode < ApplicationRecord
     belongs_to :restaurant, class_name: 'Restaurant'
     
-    scope :is_active?, -> { where("expires_on > ? OR expires_on IS NULL", DateTime.now) }
+    scope :is_active?, -> { where("expires_on >= ? OR expires_on IS NULL", 1.days.ago) }
     def self.types; ["PercentageOffBasketDiscountCode"]; end
     
     validates_presence_of :type, :code, :amount
@@ -16,7 +16,7 @@ class DiscountCode < ApplicationRecord
     def expiry_date; self.expires_on.present? ? self.expires_on.strftime("%Y-%m-%d") : "Never"; end
 
     def is_active?
-      self.expires_on.nil? || self.expires_on > DateTime.now
+      self.expires_on.nil? || self.expires_on >= 1.days.ago
     end
 
     private
