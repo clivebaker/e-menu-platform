03e3a6fbd42e9e77d9c7f881a952eef5cb7223ee
James Grant <1993jamesgrant@gmail.com>
2020-12-24 22:50:32 +0000

    Migration fix

--- db/migrate/20201209193024_change_patron_allergen_structure.rb
+++ db/migrate/20201209193024_change_patron_allergen_structure.rb
@@ -3,7 +3,5 @@ class ChangePatronAllergenStructure < ActiveRecord::Migration[6.0]
     remove_column :patron_allergens, :allergen_id
     add_reference :patron_allergens, :menu_item_categorisation, index: true, null: false
     drop_table :allergens
-
-    MenuItemCategorisation.update_all(:type=>"Allergen")
   end
 end
