beaa7bde799eea46c0777d4ce2210273b31e2c5c
James Grant <1993jamesgrant@gmail.com>
2021-01-11 14:26:00 +0000

    test connected account id

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -56,6 +56,10 @@ class Restaurant < ApplicationRecord
   def stripe_pk_api_key
     Rails.env == 'production' ? ENV['STRIPE_PK_API_KEY'] : 'pk_test_WK72bUcdjoVsncoNFQGrFkcv'
   end
+
+  def stripe_connected_account_id
+    Rails.env == 'production' ? super : 'acct_1HFMjjEdU8EUcuyG'
+  end
   
     
   def set_slug
