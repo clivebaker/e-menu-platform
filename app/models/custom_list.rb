0ab2557e36701dc4cdc8a68bf13835e6c857e9ac
level7webdev <6236517+level7webdev@users.noreply.github.com>
2021-02-24 14:54:50 +0000

    removes 'constraint' presence validation

--- app/models/custom_list.rb
+++ app/models/custom_list.rb
@@ -2,7 +2,6 @@ class CustomList < ApplicationRecord
   belongs_to :restaurant
   has_many :custom_list_items
   validates_presence_of :name, :on => [:update,:create], :message => "can't be blank"
-  validates_presence_of :constraint, :on => [:update,:create], :message => "can't be blank"
   acts_as_list scope: :restaurant
   default_scope { order(position: :asc) }
 
